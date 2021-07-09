2f7f9b9230526d3af47a469258e8cece39d82de2
Gisha <gishathedev@gmail.com>
2021-07-09 16:08:02 +0300

    improve capturing of the point

--- Tanks-Game/Assets/_Project/Scripts/Core/CapturePoint.cs
+++ Tanks-Game/Assets/_Project/Scripts/Core/CapturePoint.cs
@@ -1,34 +1,69 @@
-using System.Collections;
 using UnityEngine;
 
 namespace Gisha.TanksGame.Core
 {
     public class CapturePoint : MonoBehaviour
     {
-        public float CaptureProgress { get; private set; }
+        [SerializeField] private float fullCaptureTime = 5f;
 
-        private IEnumerator CaptureCoroutine(float operation)
+        public float CaptureProgress { private set; get; }
+
+        bool _p1Capturing = false;
+        bool _p2Capturing = false;
+
+        SpriteRenderer _spriteRenderer;
+
+        private void Awake()
         {
-            while (Mathf.Abs(CaptureProgress) < 1f)
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            // First player capturing.
+            if (_p1Capturing && !_p2Capturing)
             {
-                CaptureProgress += Time.deltaTime * operation;
-                yield return null;
+                CaptureProgress -= Time.deltaTime / fullCaptureTime;
+                if (CaptureProgress > 1f)
+                    Debug.Log("First player won!");
             }
 
-            Debug.Log("Point was captured!");
+            // Second player capturing.
+            else if (!_p1Capturing && _p2Capturing)
+            {
+                CaptureProgress += Time.deltaTime / fullCaptureTime;
+                if (CaptureProgress < -1f)
+                    Debug.Log("Second player won!");
+            }
+
+            _spriteRenderer.color = LerpColor(Color.red, Color.white, Color.blue, CaptureProgress);
+        }
+
+        private Color LerpColor(Color leftColor, Color middleColor, Color rightColor, float value)
+        {
+            float absValue = Mathf.Abs(value);
+            if (value < 0)
+                return Color.Lerp(middleColor, leftColor, absValue);
+            else if (value > 0)
+                return Color.Lerp(middleColor, rightColor, absValue);
+            else
+                return middleColor;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player1"))
-                StartCoroutine(CaptureCoroutine(-1f));
+                _p1Capturing = true;
             else if (other.CompareTag("Player2"))
-                StartCoroutine(CaptureCoroutine(1f));
+                _p2Capturing = true;
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            StopAllCoroutines();
+            if (other.CompareTag("Player1"))
+                _p1Capturing = false;
+            else if (other.CompareTag("Player2"))
+                _p2Capturing = false;
         }
     }
 }
